3d520ea164790e2f167fa5ae498dbfd16290c496
victor fernando <victorfernandomagalhaes@gmail.com>
2023-11-07 16:55:51 -0300

    fix

--- src/app/(stack)/tarefa/camera.tsx
+++ src/app/(stack)/tarefa/camera.tsx
@@ -1,11 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image, TouchableOpacity } from 'react-native';
-import { useEffect, useRef, useState } from 'react';
-import { Camera } from 'expo-camera';
-import * as MediaLibrary from 'expo-media-library';
-import { FontAwesome } from '@expo/vector-icons';
-import { router } from 'expo-router';
-import { useLocalSearchParams } from 'expo-router';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+import { useEffect, useRef, useState } from "react";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import { FontAwesome } from "@expo/vector-icons";
+import { router } from "expo-router";
+import { useLocalSearchParams } from "expo-router";
 
 export default function camera() {
   const local = useLocalSearchParams();
@@ -17,35 +25,48 @@ export default function camera() {
   useEffect(() => {
     (async () => {
       const cameraPermission = await Camera.requestCameraPermissionsAsync();
-      const mediaLibraryPermission = await MediaLibrary.requestPermissionsAsync();
+      const mediaLibraryPermission =
+        await MediaLibrary.requestPermissionsAsync();
       setHasCameraPermission(cameraPermission.status === "granted");
       setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     })();
   }, []);
 
   if (hasCameraPermission === undefined) {
-    return <Text>Requesting permissions...</Text>
+    return <Text>Requesting permissions...</Text>;
   } else if (!hasCameraPermission) {
-    return <Text>Permissão para câmera não concedida. Por favor, altere isso nas configurações.</Text>
+    return (
+      <Text>
+        Permissão para câmera não concedida. Por favor, altere isso nas
+        configurações.
+      </Text>
+    );
   }
 
   let takePic = async () => {
     let options = {
       quality: 1,
       base64: true,
-      exif: false
+      exif: false,
     };
 
     let newPhoto = await cameraRef.current.takePictureAsync(options);
     setPhoto(newPhoto);
-
   };
 
   if (photo) {
-
     let savePhoto = () => {
       MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
-        router.replace({ pathname: '/(stack)/tarefa/', params: { photoUri: photo.uri, system_type_id: local.system_type_id, client_id: local.client_id, client_parent: local.client_parent, user_id: local.user_id } });
+        router.replace({
+          pathname: "/(stack)/tarefa/",
+          params: {
+            photoUri: photo.uri,
+            system_type_id: local.system_type_id,
+            client_id: local.client_id,
+            client_parent: local.client_parent,
+            user_id: local.user_id,
+          },
+        });
         //router.replace('/(stack)/tarefa/', { photoUri: photo.uri });
         setPhoto(undefined);
       });
@@ -53,10 +74,15 @@ export default function camera() {
 
     return (
       <SafeAreaView style={styles.container}>
-        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
-        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
+        <Image
+          style={styles.preview}
+          source={{ uri: "data:image/jpg;base64," + photo.base64 }}
+        />
 
         <Button title="Discard" onPress={() => setPhoto(undefined)} />
+        <Button title="Save" onPress={savePhoto} />
+        {/* {hasMediaLibraryPermission ? (
+        ) : undefined} */}
       </SafeAreaView>
     );
   }
@@ -76,15 +102,15 @@ export default function camera() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonContainer: {
-    backgroundColor: '#fff',
-    alignSelf: 'flex-end'
+    backgroundColor: "#fff",
+    alignSelf: "flex-end",
   },
   preview: {
-    alignSelf: 'stretch',
-    flex: 1
-  }
+    alignSelf: "stretch",
+    flex: 1,
+  },
 });
